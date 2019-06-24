c66ef53c5200e53a298782d40671279b8519a6be
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-24 02:01:48 -0700

    added checkrole to auth middleware

--- middleware/auth-mw.js
+++ middleware/auth-mw.js
@@ -1,15 +1,28 @@
 const jwt = require('jsonwebtoken');
 const secrets = require('../database/secrets.js');
 
-module.exports = (req, res, next) => {
+module.exports = {
+    restricted: (req, res, next) => {
 
-    const token = req.headers.authorization;
-
-    token
-    ? jwt.verify(token, secrets.jwtSecret, (err, decodeToken) => {
-        err
-        ? res.status(401).json({ message: 'Invalid credentials.' })
-        : req.user = { roles: decodeToken.roles, username: decodeToken.username }, next()
-    })
-    : res.status(400).json({ message: 'No token provided.' });
+        const token = req.headers.authorization;
+    
+        token
+        ? jwt.verify(token, secrets.jwtSecret, (err, decodeToken) => {
+            err
+            ? res.status(401).json({ message: 'Invalid credentials.' })
+            : req.user = { roles: decodeToken.roles, username: decodeToken.username }, next()
+        })
+        : res.status(400).json({ message: 'No token provided.' });
+    },
+    checkRole: (role) => {
+        return function(req, res, next) {
+            req.user
+            ? (req.user.roles 
+               && Array.isArray(req.users.roles) 
+               && req.user.roles.includes(role)
+               ? next()
+               : res.status(403).json({ message: `You ain't authoriiiiiized sooooooonnnn!` }))
+            : res.status(401).json({ message: `Ya'll need to login first, yunno?` });
+        }
+    }
 }
